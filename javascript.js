e336ded457cfbad0eab9f1a70fb4b123eb9bdd7c
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-07-20 20:24:56 -0300

    Agregar funcionalidad d turnos

--- javascript.js
+++ javascript.js
@@ -1,39 +1,53 @@
-const Jugador = function (nombre, marcas) {
+function FabricaJugador (nombre, marca) {
     return {
         nombre,
-        marcas,
+        marca,
     }
 };
 
-const jugador1 = Jugador("Sebi", "o");
-const jugador2 = Jugador("Grili", "x");
+const jugador1 = FabricaJugador ("Sebi", "x");
+const jugador2 = FabricaJugador ("Grili", "o");
 
 const juego = (function () {
+    let jugadorEnTurno = jugador1;
+
     let arregloTablero = [".", ".", ".", ".", ".", ".", ".", ".", "."];
 
+    function dibujarTablero () {
+        let divsAremover = document.querySelectorAll(".casillero");
+        divsAremover.forEach (div => {
+            div.remove();
+        });
+        for (let casillero = 0; casillero < juego.arregloTablero.length; casillero++) {
+            const divCasillero = document.createElement("div");
+            divCasillero.setAttribute("data-casillero", `${casillero}`);
+            divCasillero.setAttribute("class", "casillero");
+            divCasillero.addEventListener("click", event => {
+                juego.jugada (event);
+            });
+            divCasillero.textContent = arregloTablero[casillero];
+            divTablero.appendChild(divCasillero);
+        };
+    };
+    
     function jugada (event) {
-        console.log(event);
-        console.log(event.target.dataset.casillero);
+        arregloTablero[event.target.dataset.casillero] = jugadorEnTurno.marca;
+        if (jugadorEnTurno === jugador1) {
+            jugadorEnTurno = jugador2;
+        } else if (jugadorEnTurno === jugador2) {
+            jugadorEnTurno = jugador1;
+        };
+        dibujarTablero();
     };
 
     return {
+        FabricaJugador,
         arregloTablero,
+        dibujarTablero,
         jugada,
     }
 })();
 
 const divTablero = document.querySelector(".tablero");
 
-for (let casillero = 0; casillero < juego.arregloTablero.length; casillero++) {
-    const divCasillero = document.createElement("div");
-    divCasillero.textContent = ".";
-    divCasillero.setAttribute("data-casillero", `${casillero}`);
-    divCasillero.addEventListener("click", event => {
-        juego.jugada (event);
-    });
-    divTablero.appendChild(divCasillero);
-}
-
-    
-
-
+juego.dibujarTablero();
